07fb70c05366808c98715e716d87a939531c0a9b
nncen <igorgetka@gmail.com>
2022-03-26 17:50:29 +0100

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 extern crate ncurses;
 
 use ncurses::*;
-int x = 5;
+
 fn main()
 {
   /* If your locale env is unicode, you should use `setlocale`. */
   // let locale_conf = LcCategory::all;
-setlocale(locale_conf, "pl_PL.UTF-8"); // if your locale is like mine(zh_CN.UTF-8).
-
+//    setlocale(locale_conf, "pl_PL.UTF-8"); // if your locale is like mine(zh_CN.UTF-8).
+ let x = 5;
   /* Start ncurses. */
   initscr();
-
   /* Print to the back buffer. */
   addstr("Hello, world!");
-  addstr(x);
   /* Print some unicode(Chinese) string. */
   /* Update the screen. */
-  refresh(
-      x = x + 1
-  );
+  refresh();
 
+  let x = x+1;
+  let s: String = x.to_string();
+  let ss: &str = &s;
+  addstr(ss);
+   
   /* Wait for a key press. */
   getch();
 
